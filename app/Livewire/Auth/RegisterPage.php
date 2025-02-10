4ea279670a9e03ed5f2fba0f53162c707a9435d4
Saw Daniel <sawdaniel@Passo.local>
2025-02-11 01:27:08 +0700

    first 19

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,43 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Register')]
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    // Regisster user
+    public function save()
+    {
+        // validate input
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:2|max:255',
+        ]);
+
+        // save user to database
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        // login user
+        auth()->login($user);
+
+        // redirect to home page
+        return redirect()->intended();
+
+    }
+
     public function render()
     {
         return view('livewire.auth.register-page');
