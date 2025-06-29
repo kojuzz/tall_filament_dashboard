e784188bae0d59982f6faeb04304e1db9fe8de8b
Ko Juzz <sawdani@gmail.com>
2025-06-29 18:20:25 +0700

    ✅ - Modify Images and Links

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -7,9 +7,11 @@
                 <div>
                     <h1
                         class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">
-                        Start your journey with <span class="text-blue-600">DCodeMania</span></h1>
-                    <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Purchase wide varities of electronics
-                        products like Smartphones, Laptops, Smartwatches, Television and many more.</p>
+                        Ecommerce Demo with <span class="text-blue-600">STRIPE Payment</span>
+                    </h1>
+                    <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">
+                        Laravel, Livewire with Filament, Tailwind CSS and Stripe Payment Gateway
+                    </p>
 
                     <!-- Buttons -->
                     <div class="mt-7 grid gap-3 w-full sm:inline-flex">
@@ -22,10 +24,10 @@
                                 <path d="m9 18 6-6-6-6" />
                             </svg>
                         </a>
-                        <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        {{-- <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                             href="/contact">
                             Contact sales team
-                        </a>
+                        </a> --}}
                     </div>
                     <!-- End Buttons -->
 
@@ -160,7 +162,7 @@
 
                 <div class="relative ms-4">
                     <img class="w-full rounded-md"
-                        src="https://static.vecteezy.com/system/resources/previews/011/993/278/non_2x/3d-render-online-shopping-bag-using-credit-card-or-cash-for-future-use-credit-card-money-financial-security-on-mobile-3d-application-3d-shop-purchase-basket-retail-store-on-e-commerce-free-png.png"
+                        src="{{ asset('images/ecommerce-slide.png') }}"
                         alt="Image Description">
                     <div
                         class="absolute inset-0 -z-[1] bg-gradient-to-tr from-gray-200 via-white/0 to-white/0 w-full h-full rounded-md mt-4 -mb-4 me-4 -ms-4 lg:mt-6 lg:-mb-6 lg:me-6 lg:-ms-6 dark:from-slate-800 dark:via-slate-900/0 dark:to-slate-900/0">
@@ -225,14 +227,15 @@
         <div class="justify-center max-w-6xl px-4 py-4 mx-auto lg:py-0">
             <div class="grid grid-cols-1 gap-6 lg:grid-cols-4 md:grid-cols-2">
 
-                @foreach ($brands as $brand)          
+                @foreach ($brands as $brand)
                     <div class="bg-white rounded-lg shadow-md dark:bg-gray-800" wire:key="{{ $brand->id }}">
                         <a href="/products?selected_brands[0]={{ $brand->id }}" class="">
-                            <img src="{{ url('storage', $brand->image) }}"
-                                alt="{{ $brand->name }}" class="object-cover w-full h-64 rounded-t-lg">
+                            <img src="{{ url("storage", $brand->image) }}" alt="{{ $brand->name }}"
+                                class="object-cover w-full h-64 rounded-t-lg">
                         </a>
                         <div class="p-5 text-center">
-                            <a href="" class="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-300">
+                            <a href=""
+                                class="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-300">
                                 {{ $brand->name }}
                             </a>
                         </div>
@@ -278,8 +281,7 @@
                             <div class="flex justify-between items-center">
                                 <div class="flex items-center">
                                     <img class="h-[2.375rem] w-[2.375rem] rounded-full object-contain"
-                                        src="{{ url('storage', $category->image) }}"
-                                        alt="{{ $category->name }}">
+                                        src="{{ url("storage", $category->image) }}" alt="{{ $category->name }}">
                                     <div class="ms-3">
                                         <h3
                                             class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
@@ -288,9 +290,10 @@
                                     </div>
                                 </div>
                                 <div class="ps-3">
-                                    <svg class="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24"
-                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg class="flex-shrink-0 w-5 h-5" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round">
                                         <path d="m9 18 6-6-6-6" />
                                     </svg>
                                 </div>
@@ -305,7 +308,7 @@
 
     </div>
     {{-- Category Section End --}}
-    
+
     {{-- Customer Review Section Start --}}
     {{-- <section class="py-14 font-poppins dark:bg-gray-800">
         <div class="max-w-6xl px-4 py-6 mx-auto lg:py-4 md:px-6">
